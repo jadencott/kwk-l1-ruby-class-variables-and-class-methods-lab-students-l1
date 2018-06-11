753ebc2b68a25c5d81ae5c29bf994fd9c3ac7d4d
Jaden <jcottorone21405@gmail.com>
2018-06-11 15:57:28 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,12 @@
+class Song (name, artist, genre)
+  def initialize 
+    @name = name
+    @artist = artist 
+    @genre = genre
+end 
+  
+  
+  
+  
+  ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "Rap")
 
